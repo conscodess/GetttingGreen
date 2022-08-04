90e5daca585d92b8681affd59c4dbb52d5d92be2
Olivia Yang <oliviadanyang@gmail.com>
2022-08-04 11:13:49 -0500

    build that score landing page

--- GetttingGreen/ViewController.swift
+++ GetttingGreen/ViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreReportButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
-
+    @IBAction func scoreReportButton(_ sender: Any) {
+        let url = URL(string:
+        "https://www.annualcreditreport.com/requestReport/landingPage.action")!
+        UIApplication.shared.open (url)
+    }
+    
 }
 
 

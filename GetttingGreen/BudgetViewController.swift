4a0236ea117b2e8f32b637126154b1592eec1051
conscodess <91997150+conscodess@users.noreply.github.com>
2022-08-04 19:31:08 -0500

    almost all done, just missing the extra resources and buttons on crreditt builderr

--- GetttingGreen/BudgetViewController.swift
+++ GetttingGreen/BudgetViewController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
+
+
+
 class BudgetViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var necessitiesLabel: UILabel!
+    @IBOutlet weak var wantsLabel: UILabel!
+    @IBOutlet weak var savingsLabel: UILabel!
+    
+    @IBAction func enterTapped(_ sender: Any) {
+        
+        let income = Double(textField.text!) ?? 0.0
+                let necessities = Int(ceil(income*0.5))
+                let wants = Int(ceil(income*0.3))
+                let savings = Int(ceil(income*0.2))
+                necessitiesLabel.text = "$" + String(necessities)
+                wantsLabel.text = "$" + String(wants)
+                savingsLabel.text = "$" + String(savings)
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
